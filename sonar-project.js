9c31b212d767804eb9242fcf5c3b2cc0f618adf2
fazilniveus <73817323+fazilniveus@users.noreply.github.com>
2022-12-02 11:25:46 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -9,8 +9,8 @@ sonarqubeScanner(
       'sonar.exclusions': '**/*.test.*',
       'sonar.inclusions' :  '**',
       'sonar.test.inclusions':  '__tests__/**/*.test.js',
-      'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
-      'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml'
+      'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info'
+      '
       
     }
   }, () => {});
